d840836936618562a2c32789cfed7094f5b3a3d7
Eric Hanchrow <eric.hanchrow@gmail.com>
2009-02-15 14:22:44 -0800

    Basic auth in Python

---
+++ auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+"""
+Authenticate against Google's Calendar APIs.  This requires that you
+download and install
+http://gdata-python-client.googlecode.com/files/gdata.py-1.2.4.tar.gz
+or something quite similar.
+"""
+
+from __future__ import with_statement
+__author__ = ""
+__version__ = "$Id$"
+
+from xml.etree import ElementTree
+import atom
+import atom.service
+import gdata.calendar
+import gdata.calendar.service
+import gdata.service
+import getopt
+import os
+import re
+import string
+import sys
+import time
+
+def auth(email, password):
+    calendar_service = gdata.calendar.service.CalendarService()
+    calendar_service.email = email
+    calendar_service.password = password
+    calendar_service.source = "eric.hanchrow-gcaldeduplicator-version0"
+    calendar_service.ProgrammaticLogin()
+
+def snag_local_auth_info():
+    with open(os.path.join(os.getenv("HOME"), ".imap-authinfo")) as fh:
+        for line in fh:
+            m = re.match(".*login (.*?) password \"(.*)\".*", line)
+            if m:
+                return list(m.groups())
+    return None
+
+if __name__ == "__main__":
+    auth(*snag_local_auth_info())
